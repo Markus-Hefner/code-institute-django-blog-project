2a4ffa493302b3a9b9e16569b8539cdea1127a0b
Markus Hefner <m5zrlnlt@students.codeinstitute.net>
2025-02-08 10:32:34 +0000

    Building the Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-markushefne-codeinstitu-z0o9c6oelhx.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "postgresql://neondb_owner:npg_KXWG0tBrI7Yi@ep-crimson-term-a26m6iqc.eu-central-1.aws.neon.tech/graph_left_award_676673"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
