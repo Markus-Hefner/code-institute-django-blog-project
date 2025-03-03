518252d00d1dcf2ba4c531778568528c3a7d1857
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2025-03-03 11:01:06 +0100

    Add collaborate request form to about page (front- and backend - done)

--- about/views.py
+++ about/views.py
@@ -14,15 +14,7 @@ def about_me(request):
         print("Received a POST request")
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
-            print(collaborate_form)
-
-            # collaborate_request = collaborate_form.save(commit=False)
-            # collaborate_request.name = collaborate_form.name
-            # collaborate_request.email = collaborate_form.email
-            # collaborate_request.message = collaborate_form.message
-            # collaborate_request.read = collaborate_form.read
-            # collaborate_request.save
-            collaborate_form.save
+            collaborate_form.save()
             messages.add_message(
                 request, messages.SUCCESS,
                 'Collaboration request received! I endeavour to respond within 2 working days.'
